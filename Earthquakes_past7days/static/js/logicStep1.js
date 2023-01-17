89f833c21c050e971de5dcf3b04248b003967953
Nicholas-DeMarco <GCHockey05@hotmail.com>
2023-01-17 16:49:45 -0500

    Adding updated files to Earthquakes_past7days.

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -20,16 +20,16 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
-    "Satellite Streets": satelliteStreets
+    "Satellite": satelliteStreets
   };
 
 // Create the map object with center, zoom level and default layer.
 
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
-})
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+});
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -38,13 +38,9 @@ L.control.layers(baseMaps).addTo(map);
 
 streets.addTo(map);
 
-// Accessing the airport GeoJSON URL
+// Retrieve the earthquake GeoJSON data.
 
-let torontoHoods = "https://raw.githubusercontent.com/Nicholas-DeMarco/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
 });
